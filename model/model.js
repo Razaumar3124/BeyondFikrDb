5cf48df38e8251ad81c72e3d7f7ec181223c18cf
Abdurrazzak Rauff Shaikh <152030209+Razaumar3124@users.noreply.github.com>
2025-07-22 22:35:10 +0530

    Add missing model.js file with User and Blog schemas

--- model/model.js
+++ model/model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
-  contact: { type: String, required: true },
+  contact: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   date: { type: Date, default: Date.now },
   isAdmin: { type: Boolean, default: false },
