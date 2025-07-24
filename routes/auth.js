df1f83c8380aa13b6582cd7dd8289cec661f47c0
Abdurrazzak Rauff Shaikh <152030209+Razaumar3124@users.noreply.github.com>
2025-07-24 20:07:57 +0530

    changes made in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const crypto = require("crypto");
 const nodemailer = require("nodemailer");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { User } = require("../model/model");
 
 // Forgot Password
